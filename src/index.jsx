09cded6f6729fe6866eacaea11832961cdf8cb14
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-06-18 08:51:56 -0300

    creating store and reducers on index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 import App from './main/app'
+import reducers from './main/reducers'
 
+const store = createStore(reducers)
 
-
-ReactDOM.render(<App />, document.getElementById('app')
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  ,document.getElementById('app')
 )
