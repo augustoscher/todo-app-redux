566b8e9f8e5aa51775f3730c261cdbd4723fbdba
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-06-21 13:57:45 -0300

    87 - adding middleware to get todo list from backend

--- src/todo/todoReducer.jsx
+++ src/todo/todoReducer.jsx
@@ -1,20 +1,6 @@
 const INITIAL_STATE = {
-    description: 'Ler livro',
-    list: [{
-        _id: 1,
-        description: 'Pagar cartão',
-        done: true
-    },
-    {
-        _id: 2,
-        description: 'Reunião com a equipe as 10:00',
-        done: false
-    },
-    {
-        _id: 3,
-        description: 'Consulta médica na terça',
-        done: false
-    }]
+    description: '',
+    list: []
 }
 
 //Reducer sempre recebe o estado atual do objeto e uma action;
